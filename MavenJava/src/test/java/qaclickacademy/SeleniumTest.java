c600744b2a8928d066550b94ec2729b8b4c3cb0b
Udit-das <uditnarayandasp@gmail.com>
2019-10-15 18:51:45 +0530

    first commit

---
+++ MavenJava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+		//WebDriver driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+}
